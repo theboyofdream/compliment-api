ba9308c9825297636006d20cc6d1020ac078ede2
theboyofdream <infinityknight024@gmail.com>
2024-10-18 22:54:28 +0530

    url update

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const compliment = require("./complimentr");
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app
-  .get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./index.html"));
-  })
-  .get("/public/randomcompliment/compliment/random", (req, res) => {
-    res.send(compliment());
-  });
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+
+app.get("/public/randomcompliment/compliment/random", (req, res) =>
+  res.send(compliment())
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
